c8a59beb06778371fb7f4af42b903652690e6cc2
Boon Jhee <wbjhee@gmail.com>
2021-11-30 13:23:59 +0800

    best possible optimization?

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int line( int x0, int x1, int y0, int y1, TGAImage &image, TGAColor color){
     bool steep = false;
     if(std::abs(x0-x1)< std::abs(y0-y1)){
         std::swap(x0, y0);
-        std::swap(x1,y1);
+        std::swap(x1, y1);
         steep = true;
     }
 
@@ -25,15 +25,15 @@ int line( int x0, int x1, int y0, int y1, TGAImage &image, TGAColor color){
 
     int dx = x1-x0;
     int dy = y1-y0;
-    float gradient = std::abs(dy/dx);
-    float ystep = 0;
+    int dy_doubled = 2*dy; //remove floats
+    int ystep = 0;
     int y = y0;
     for (float x=x0; x<=x1; x++){
         steep == false ? image.set(x,y,color): image.set(y,x,color);
-        ystep += gradient;
-        if (ystep>.5){
+        ystep += dy_doubled;
+        if (ystep> dx*2){
             y += (y1>y0?1:-1);
-            ystep -= 1.;
+            ystep -= 2*dx;
         }
     }
 }
